963083c03eba3ceb240febf768aa81471c759967
alana0vit <alana.vit.ms@gmail.com>
2026-03-08 22:37:40 -0300

    Configurando a estrutura inicial do projeto

---
+++ src/test/java/br/com/conectaPro/ConectaProApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.conectaPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaProApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
