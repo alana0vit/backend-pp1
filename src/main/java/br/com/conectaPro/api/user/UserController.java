2ae4b56cb8a87becd87683be7f335147c7fbfec5
Lucas Gabriel <>
2026-04-11 10:43:35 -0300

    feat: adição de list, listAll, update e delete da entidade user (sem as regras de negocio)

--- src/main/java/br/com/conectaPro/api/user/UserController.java
+++ src/main/java/br/com/conectaPro/api/user/UserController.java
@@ -4,10 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import br.com.conectaPro.model.user.User;
 import br.com.conectaPro.model.user.UserService;
@@ -24,5 +30,30 @@ public class UserController {
 
         User user = userService.save(request.build());
         return new ResponseEntity<User>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable("id") Long id, @RequestBody UserRequest request) {
+
+        userService.update(id, request.build());
+        return ResponseEntity.ok().build();
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        userService.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/br/com/conectaPro/model/user/UserService.java
+++ src/main/java/br/com/conectaPro/model/user/UserService.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
 
+import java.util.List;
+
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository repository;
 
@@ -17,4 +19,40 @@ public class UserService {
         user.setEnabled(Boolean.TRUE);
         return repository.save(user);
     }
+
+    public List<User> getAll() {
+
+        return repository.findAll();
+    }
+
+    public User getById(Long id) {
+
+        return repository.findById(id).get();
+    }
+
+    @Transactional
+    public void update(Long id, User userChanged) {
+
+        User user = repository.findById(id).get();
+        user.setName(userChanged.getName());
+        user.setEmail(userChanged.getEmail());
+        user.setPassword(userChanged.getPassword());
+        user.setCpf_cnpj(userChanged.getCpf_cnpj());
+        user.setBirthDate(userChanged.getBirthDate());
+        user.setUserType(userChanged.getUserType());
+        user.setAddressId(userChanged.getAddressId());
+        user.setActive(userChanged.getActive());
+
+        repository.save(user);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+
+        User user = repository.findById(id).get();
+        user.setEnabled(Boolean.FALSE);
+
+        repository.save(user);
+    }
+
 }
