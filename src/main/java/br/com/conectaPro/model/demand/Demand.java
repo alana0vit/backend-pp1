f485eb09c1039345936fcf9ce8bdc042f70bc8f8
Lucas Gabriel <>
2026-04-11 11:48:33 -0300

    feat: preenchimento da classe demand (sem relacionamento)

--- src/main/java/br/com/conectaPro/model/demand/Demand.java
+++ src/main/java/br/com/conectaPro/model/demand/Demand.java
@@ -1,5 +1,49 @@
 package br.com.conectaPro.model.demand;
 
-public class Demand {
+import org.hibernate.annotations.SQLRestriction;
+
+import br.com.conectaPro.util.entity.AudibleEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Demand")
+@SQLRestriction("enabled = true")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Demand extends AudibleEntity{
+
+    @Column(unique = true)
+    private String code;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String descricao;
+
+    @Column
+    private String imgUrl;
+
+    @Column
+    private Long addressId;
+
+    @Column
+    private Long categoryId;
+
+    @Column
+    private Long clientId;
+
+    @Column
+    private Long professionalId;
     
 }
