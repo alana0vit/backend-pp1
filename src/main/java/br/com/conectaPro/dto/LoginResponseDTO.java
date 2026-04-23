60caab001d0c31638efe30d3d0ab0c8a89aaef65
João Pedro da Costa <jpcc.dev@gmail.com>
2026-04-23 20:35:24 -0300

    fix: typo no token

--- src/main/java/br/com/conectaPro/dto/LoginResponseDTO.java
+++ src/main/java/br/com/conectaPro/dto/LoginResponseDTO.java
@@ -3,7 +3,7 @@ package br.com.conectaPro.dto;
 import br.com.conectaPro.model.user.UserType;
 
 public record LoginResponseDTO(
-        String tooken,
+        String token,
         Long id,
         String name,
         UserType userType) {
