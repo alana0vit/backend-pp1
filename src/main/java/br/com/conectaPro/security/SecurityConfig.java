cac7b17595931687311d53050739bd50b2fe54d2
João Pedro da Costa <jpcc.dev@gmail.com>
2026-04-23 23:26:01 -0300

    fix: erros de cors no front

--- src/main/java/br/com/conectaPro/security/SecurityConfig.java
+++ src/main/java/br/com/conectaPro/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package br.com.conectaPro.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -42,11 +47,35 @@ public class SecurityConfig {
         // return http.build();
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configure(http))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // Libera todas as rotas sem exceção
                 );
         return http.build();
+    }
+
+    // Define as regras de quem pode acessar a API
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // Permite a origem do seu front-end (Vite)
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+
+        // Permite os métodos HTTP principais
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+
+        // Permite todos os headers (importante para enviar o Content-Type e
+        // Authorization)
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        // Permite o envio de cookies ou credenciais se necessário
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // Aplica essa configuração em todos os endpoints da API
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
