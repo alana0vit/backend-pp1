022bbccac8271b3aaeeed4f58276b656c45e5ee9
João Pedro da Costa <59283595+joaopedrodacostacarvalho@users.noreply.github.com>
2026-06-07 11:31:03 -0300

    Fix typo in DemandStatus enum from AGURADANDO to AGUARDANDO

--- src/main/java/br/com/conectaPro/model/demand/DemandStatus.java
+++ src/main/java/br/com/conectaPro/model/demand/DemandStatus.java
@@ -3,7 +3,7 @@ package br.com.conectaPro.model.demand;
 public enum DemandStatus {
     // aberto (enviou pro profissional), aguardando (profissional aceitou e esta fazendo ou ja fez), fechado (avaliou) e rejeitado (caso ele nao queira fazer)
     ABERTO, // Ao ser enviada ao profissional
-    AGURADANDO, // Profissional aceitou, está fazendo ou já fez, 
+    AGUARDANDO, // Profissional aceitou, está fazendo ou já fez, 
     FECHADO, // Feita e avaliada
     REJEITADO // Rejeitada, caso ele não queira fazer, notifica o usuário e pode voltar ficar como aberta?
 }
